658c34bcfbe7b5c6578bcdefa5080e5e761c6a8e
yeongwoo <jyy0223@naver.com>
2024-06-26 21:49:29 +0900

    [feat] PT Detail Model을 ShopInfo와 SellerInfo 1:1 로 갖도록 분기

--- lib/personal_training/model/personal_training_detail_model.dart
+++ lib/personal_training/model/personal_training_detail_model.dart
@@ -1,21 +1,8 @@
 import 'package:cafe/personal_training/model/personal_training_model.dart';
 
 class PersonalTrainingDetailModel extends PersonalTrainingModel {
-  // PT샵 정보
-  final String businessType; // 업종
-  final String roadNameAddress; // 도로명 주소
-  final String localAddress; // 지번 주소
-  final int premium; // 권리금
-  final int deposit; // 보증금
-  final int monthlyRent; // 월세
-  final int administrationCost; // 관리비
-
-  // 판매자 정보
-  final String name; // 사업 명칭
-  final String locationOfBuilding; // 소재지
-  final String registrationNumber; // 등록 번호
-  final String representative; // 대표자
-  final String contactInfo; // 연락처
+  final ShopInfo shop;
+  final SellerInfo seller;
 
   PersonalTrainingDetailModel({
     required super.id,
@@ -27,6 +14,21 @@ class PersonalTrainingDetailModel extends PersonalTrainingModel {
     required super.openingHours,
     required super.location,
     required super.categoryStatus,
+    required this.shop,
+    required this.seller,
+  });
+}
+
+class ShopInfo {
+  final String businessType; // 업종
+  final String roadNameAddress; // 도로명 주소
+  final String localAddress; // 지번 주소
+  final int premium; // 권리금
+  final int deposit; // 보증금
+  final int monthlyRent; // 월세
+  final int administrationCost;
+
+  ShopInfo({
     required this.businessType,
     required this.roadNameAddress,
     required this.localAddress,
@@ -34,10 +36,21 @@ class PersonalTrainingDetailModel extends PersonalTrainingModel {
     required this.deposit,
     required this.monthlyRent,
     required this.administrationCost,
+  }); // 관리비
+}
+
+class SellerInfo {
+  final String name; // 사업 명칭
+  final String locationOfBuilding; // 소재지
+  final String registrationNumber; // 등록 번호
+  final String representative; // 대표자
+  final String contactInfo;
+
+  SellerInfo({
     required this.name,
     required this.locationOfBuilding,
     required this.registrationNumber,
     required this.representative,
     required this.contactInfo,
-  });
+  }); // 연락처
 }
