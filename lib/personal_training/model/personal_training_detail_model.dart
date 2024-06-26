47c67920d9eba9c8086bf9dd3302468661b29723
yeongwoo <jyy0223@naver.com>
2024-06-26 21:45:03 +0900

    [feat] PersonalTrainingDetailModel 생성 후 초기 데이터 교체

--- lib/personal_training/model/personal_training_detail_model.dart
+++ lib/personal_training/model/personal_training_detail_model.dart
@@ -1,6 +1,22 @@
 import 'package:cafe/personal_training/model/personal_training_model.dart';
 
 class PersonalTrainingDetailModel extends PersonalTrainingModel {
+  // PT샵 정보
+  final String businessType; // 업종
+  final String roadNameAddress; // 도로명 주소
+  final String localAddress; // 지번 주소
+  final int premium; // 권리금
+  final int deposit; // 보증금
+  final int monthlyRent; // 월세
+  final int administrationCost; // 관리비
+
+  // 판매자 정보
+  final String name; // 사업 명칭
+  final String locationOfBuilding; // 소재지
+  final String registrationNumber; // 등록 번호
+  final String representative; // 대표자
+  final String contactInfo; // 연락처
+
   PersonalTrainingDetailModel({
     required super.id,
     required super.title,
@@ -11,5 +27,17 @@ class PersonalTrainingDetailModel extends PersonalTrainingModel {
     required super.openingHours,
     required super.location,
     required super.categoryStatus,
+    required this.businessType,
+    required this.roadNameAddress,
+    required this.localAddress,
+    required this.premium,
+    required this.deposit,
+    required this.monthlyRent,
+    required this.administrationCost,
+    required this.name,
+    required this.locationOfBuilding,
+    required this.registrationNumber,
+    required this.representative,
+    required this.contactInfo,
   });
 }
--- lib/personal_training/provider/personal_training_provider.dart
+++ lib/personal_training/provider/personal_training_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cafe/personal_training/model/personal_training_detail_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../common/const/image_path.dart';
@@ -5,11 +6,11 @@ import '../../common/utils/data_utils.dart';
 import '../model/personal_training_model.dart';
 
 final personalTrainingDetailProvider =
-    Provider.family<PersonalTrainingModel, String>((ref, id) {
+    Provider.family<PersonalTrainingDetailModel, String>((ref, id) {
   final pt = ref
       .watch(personalTrainingProvider)
       .firstWhere((element) => element.id == id);
-  return pt;
+  return pt as PersonalTrainingDetailModel;
 });
 
 final personalTrainingProvider = StateNotifierProvider<
@@ -26,7 +27,7 @@ class PersonalTrainingStateNotifier
   List<PersonalTrainingModel> getItems() {
     return List.generate(
       6,
-      (index) => PersonalTrainingModel(
+      (index) => PersonalTrainingDetailModel(
         id: DataUtils.getUuid(),
         title: '피트니스 파워 하우스',
         description: '동해물과 백두산이 마르고 닳도록 하느님.asdfasdfasdf..',
@@ -36,6 +37,18 @@ class PersonalTrainingStateNotifier
         openingHours: '11:00 ~ 20:00',
         location: '서울특별시 종로구 세종대로 172',
         categoryStatus: CategoryStatus.special,
+        businessType: '헬스클럽',
+        roadNameAddress: '서울특별시 관악구 난곡로 84 (신림동)',
+        localAddress: '서울특별시 관악구 신림동 668-11 큰믿음유신교회',
+        premium: 14000,
+        deposit: 5000,
+        monthlyRent: 400,
+        administrationCost: 50,
+        name: '헬스클럽',
+        locationOfBuilding: '서울특별시 관악구 난곡로 84 (신림동)',
+        registrationNumber: '1310424302918',
+        representative: '김세준',
+        contactInfo: '01012341234',
       ),
     );
   }
