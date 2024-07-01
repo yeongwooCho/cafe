93c975b79e724fff141e3d5f5ad5ec7192b97418
yeongwoo <jyy0223@naver.com>
2024-07-01 15:03:25 +0900

    [feat] 상품 데이터 추가 및 상품 등록하기 버튼 추가

--- lib/product/view/my_product_list_screen.dart
+++ lib/product/view/my_product_list_screen.dart
@@ -1,3 +1,6 @@
+import 'package:cafe/common/component/show/show_custom_toast.dart';
+import 'package:cafe/common/const/colors.dart';
+import 'package:cafe/common/const/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,14 +18,30 @@ class MyProductListScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final products = ref
         .watch(productProvider)
-        .where((element) => element.id % 4 == 0)
+        .where((element) => element.id % 2 == 0)
         .toList();
 
     return DefaultLayout(
-      appbar: const DefaultAppBar(title: '내 상품'),
-      child: Expanded(
-        child: VerticalItemGrid(products: products),
+      appbar: DefaultAppBar(
+        title: '내 상품',
+        action: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: TextButton(
+              onPressed: () {
+                showCustomToast(context, msg: '서비스 준비중입니다.');
+              },
+              child: Text(
+                '상품등록하기',
+                style: MyTextStyle.descriptionRegular.copyWith(
+                  color: MyColor.primary,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
+      child: VerticalItemGrid(products: products),
     );
   }
 }
